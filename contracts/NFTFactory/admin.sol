bf7407bfe7b49666e6ba92f70d4bad0b4cae4e6f
jangju jo <team.gutalik@gmail.com>
2022-10-25 01:39:58 +0900

    NFT contract setting

---
+++ contracts/NFTFactory/admin.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v4.5.0) (token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol)
+
+pragma solidity ^0.8.0;
+
+
+contract admin {
+    
+}
